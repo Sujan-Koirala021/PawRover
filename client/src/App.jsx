5b9f3406c7e9c4ec728150184d9d05566a618b35
Sujan <077bct087.sujan@pcampus.edu.np>
2024-05-30 17:49:18 +0545

    react tailwind setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='underline text-red-600 pt-24'>Hello MY frands</div>
+    </>
+  )
+}
+
+export default App
