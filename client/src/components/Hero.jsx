5568bc785a4d0b115962d490add03c2a1939b5f0
Sujan <077bct087.sujan@pcampus.edu.np>
2024-06-02 23:50:25 +0545

    Tour init + style search

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+// Import necessary dependencies
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import heroImage from '../assets/images/pet-sitter-hero.jpg';
+import tortoiseImg from '../assets/images/turtle2.gif';
+import rabbitImg from '../assets/images/rabbit1.gif';
+import Shepherd from 'shepherd.js';
+import 'shepherd.js/dist/css/shepherd.css';
+import './HeroTour.css'; // Import CSS file for tour styling
 
 const Hero = () => {
+    useEffect(() => {
+        const tour = new Shepherd.Tour({
+            defaultStepOptions: {
+                classes: 'shadow-md bg-purple-dark',
+                scrollTo: { behavior: 'smooth', block: 'center' }
+            },
+            useModalOverlay: true
+        });
+
+        tour.addStep({
+            id: 'welcome',
+            text: `
+                <div class="tour-step">
+                    <img src="${tortoiseImg}" alt="Tortoise" class="tour-image">
+                    <div>
+                        <h3>Welcome! I am Turbo the Tortoise.</h3>
+                        <p>Yes, I'm the one who beat the rabbit in the race. Let's see if I can guide you through Paw Rover faster than Speedy!</p>
+                    </div>
+                </div>
+            `,
+            buttons: [
+                {
+                    text: 'Next',
+                    action: tour.next
+                }
+            ]
+        });
+
+        // Add more steps for the tour
+
+        tour.start();
+    }, []);
+
     return (
         <div
             className="bg-cover bg-center relative"
@@ -11,22 +50,14 @@ const Hero = () => {
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="container mx-auto flex flex-col justify-center text-white relative z-10 h-full">
                 <div className="text-left">
-
                     <h1 className="text-4xl md:text-6xl font-bold mb-10">Paw Rover</h1>
-                    <p className="text-lg md:text-xl max-w-md">Uncover special homestays across the globe, exchanging care for adorable pets for a free stay.
-                    </p>
-                    {/* <Link to='/home'> */}
+                    <p className="text-lg md:text-xl max-w-md">Uncover special homestays across the globe, exchanging care for adorable pets for a free stay.</p>
                     <Link to='/find-pet-sitter'>
-
                         <button className="hero-button mr-12">Find a Pet Sitter &gt;</button>
                     </Link>
                     <Link to='/find-house-sit'>
                         <button className="hero-button ml-6">Explore Homes to Sit &gt;</button>
-
                     </Link>
-
-
-                    {/* </Link> */}
                 </div>
             </div>
         </div>
--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 // import { FaMapMarkerAlt, FaStar, FaUser } from 'react-icons/fa';
 import { FaMapMarkerAlt, FaStar, FaUser, FaPhone } from 'react-icons/fa';
 
-
+import avatar from '../assets/images/avatar.png'
 const SearchResults = () => {
   const location = useLocation();
   const { state } = location;
@@ -40,40 +40,40 @@ const SearchResults = () => {
     { id: 28, name: 'Dorothy Gonzalez', description: 'Cat and small mammals lover', pets: ['Cat', 'Hamster'], rating: 4.8, location: 'Las Vegas' },
     { id: 29, name: 'Kenneth Nelson', description: 'Specializes in reptiles', pets: ['Snake', 'Lizard'], rating: 4.7, location: 'Louisville' },
     { id: 30, name: 'Sandra Carter', description: 'Experienced with farm animals', pets: ['Horse', 'Goat'], rating: 4.9, location: 'Baltimore' },
-    { id: 31, name: 'Joshua Mitchell', description: 'Birds and small pets expert', pets: ['Bird', 'Hamster'], rating: 4.6, location: 'Milwaukee' },
-    { id: 32, name: 'Michelle Perez', description: 'Dog and cat lover', pets: ['Dog', 'Cat'], rating: 4.8, location: 'Albuquerque' },
-    { id: 33, name: 'Kevin Roberts', description: 'Reptiles and amphibians care', pets: ['Turtle', 'Snake'], rating: 4.7, location: 'Tucson' },
-    { id: 34, name: 'Mary Turner', description: 'Small mammals and birds enthusiast', pets: ['Rabbit', 'Bird'], rating: 4.9, location: 'Fresno' },
-    { id: 35, name: 'Edward Phillips', description: 'Experienced with aquatic pets', pets: ['Fish'], rating: 4.6, location: 'Sacramento' },
-    { id: 36, name: 'Linda Campbell', description: 'Cats and small animals sitter', pets: ['Cat', 'Guinea Pig'], rating: 4.8, location: 'Mesa' },
-    { id: 37, name: 'Thomas Parker', description: 'Dogs and exotic pets expert', pets: ['Dog', 'Snake'], rating: 4.7, location: 'Kansas City' },
-    { id: 38, name: 'Lisa Evans', description: 'Passionate about birds', pets: ['Bird'], rating: 4.9, location: 'Atlanta' },
-    { id: 39, name: 'Joseph Edwards', description: 'Specializes in reptiles', pets: ['Lizard', 'Snake'], rating: 4.8, location: 'Omaha' },
-    { id: 40, name: 'Jennifer Collins', description: 'Small mammals and birds enthusiast', pets: ['Hamster', 'Bird'], rating: 4.7, location: 'Colorado Springs' },
-    { id: 41, name: 'Anthony Stewart', description: 'Experienced with cats and dogs', pets: ['Cat', 'Dog'], rating: 4.9, location: 'Raleigh' },
-    { id: 42, name: 'Patricia Sanchez', description: 'Reptiles and amphibians care', pets: ['Turtle', 'Frog'], rating: 4.6, location: 'Miami' },
-    { id: 43, name: 'Andrew Morris', description: 'Loves taking care of rabbits', pets: ['Rabbit'], rating: 4.8, location: 'Long Beach' },
-    { id: 44, name: 'Laura Rogers', description: 'Expert in exotic pets', pets: ['Snake', 'Lizard'], rating: 4.7, location: 'Virginia Beach' },
-    { id: 45, name: 'Christopher Reed', description: 'Dog and bird care enthusiast', pets: ['Dog', 'Bird'], rating: 4.9, location: 'Oakland' },
-    { id: 46, name: 'Sarah Cook', description: 'Experienced with aquatic pets', pets: ['Fish'], rating: 4.6, location: 'Minneapolis' },
-    { id: 47, name: 'David Bell', description: 'Cat and small mammals lover', pets: ['Cat', 'Hamster'], rating: 4.8, location: 'Tulsa' },
-    { id: 48, name: 'Susan Murphy', description: 'Specializes in reptiles', pets: ['Snake', 'Lizard'], rating: 4.7, location: 'Arlington' },
-    { id: 49, name: 'James Bailey', description: 'Experienced with farm animals', pets: ['Horse', 'Goat'], rating: 4.9, location: 'Tampa' },
-    { id: 50, name: 'Karen Rivera', description: 'Birds and small pets expert', pets: ['Bird', 'Hamster'], rating: 4.6, location: 'New Orleans' },
-    { id: 51, name: 'Nancy Brooks', description: 'Experienced with cats and dogs', pets: ['Cat', 'Dog'], rating: 4.8, location: 'Wichita' },
-    { id: 52, name: 'Gregory Powell', description: 'Professional sitter for exotic pets', pets: ['Snake', 'Fish'], rating: 4.9, location: 'Bakersfield' },
-    { id: 53, name: 'Angela Bennett', description: 'Great with reptiles and amphibians', pets: ['Turtle', 'Lizard'], rating: 4.7, location: 'Cleveland' },
-    { id: 54, name: 'George Russell', description: 'Loves taking care of birds and rabbits', pets: ['Bird', 'Rabbit'], rating: 4.6, location: 'Aurora' },
-    { id: 55, name: 'Sharon Fisher', description: 'Dog and cat lover', pets: ['Dog', 'Cat'], rating: 4.8, location: 'Anaheim' },
-    { id: 56, name: 'Patrick Peterson', description: 'Expert in exotic animals', pets: ['Snake', 'Lizard'], rating: 4.9, location: 'Santa Ana' },
-    { id: 57, name: 'Michelle Simmons', description: 'Birds and small animals enthusiast', pets: ['Bird', 'Hamster'], rating: 4.6, location: 'Corpus Christi' },
-    { id: 58, name: 'Larry Richardson', description: 'Experienced with farm animals', pets: ['Horse', 'Goat'], rating: 4.8, location: 'Riverside' },
-    { id: 59, name: 'Sandra Hughes', description: 'Small mammals and birds lover', pets: ['Rabbit', 'Bird'], rating: 4.7, location: 'Lexington' },
-    { id: 60, name: 'Timothy Jenkins', description: 'Professional sitter for aquatic pets', pets: ['Fish'], rating: 4.9, location: 'Stockton' },
+    // { id: 31, name: 'Joshua Mitchell', description: 'Birds and small pets expert', pets: ['Bird', 'Hamster'], rating: 4.6, location: 'Milwaukee' },
+    // { id: 32, name: 'Michelle Perez', description: 'Dog and cat lover', pets: ['Dog', 'Cat'], rating: 4.8, location: 'Albuquerque' },
+    // { id: 33, name: 'Kevin Roberts', description: 'Reptiles and amphibians care', pets: ['Turtle', 'Snake'], rating: 4.7, location: 'Tucson' },
+    // { id: 34, name: 'Mary Turner', description: 'Small mammals and birds enthusiast', pets: ['Rabbit', 'Bird'], rating: 4.9, location: 'Fresno' },
+    // { id: 35, name: 'Edward Phillips', description: 'Experienced with aquatic pets', pets: ['Fish'], rating: 4.6, location: 'Sacramento' },
+    // { id: 36, name: 'Linda Campbell', description: 'Cats and small animals sitter', pets: ['Cat', 'Guinea Pig'], rating: 4.8, location: 'Mesa' },
+    // { id: 37, name: 'Thomas Parker', description: 'Dogs and exotic pets expert', pets: ['Dog', 'Snake'], rating: 4.7, location: 'Kansas City' },
+    // { id: 38, name: 'Lisa Evans', description: 'Passionate about birds', pets: ['Bird'], rating: 4.9, location: 'Atlanta' },
+    // { id: 39, name: 'Joseph Edwards', description: 'Specializes in reptiles', pets: ['Lizard', 'Snake'], rating: 4.8, location: 'Omaha' },
+    // { id: 40, name: 'Jennifer Collins', description: 'Small mammals and birds enthusiast', pets: ['Hamster', 'Bird'], rating: 4.7, location: 'Colorado Springs' },
+    // { id: 41, name: 'Anthony Stewart', description: 'Experienced with cats and dogs', pets: ['Cat', 'Dog'], rating: 4.9, location: 'Raleigh' },
+    // { id: 42, name: 'Patricia Sanchez', description: 'Reptiles and amphibians care', pets: ['Turtle', 'Frog'], rating: 4.6, location: 'Miami' },
+    // { id: 43, name: 'Andrew Morris', description: 'Loves taking care of rabbits', pets: ['Rabbit'], rating: 4.8, location: 'Long Beach' },
+    // { id: 44, name: 'Laura Rogers', description: 'Expert in exotic pets', pets: ['Snake', 'Lizard'], rating: 4.7, location: 'Virginia Beach' },
+    // { id: 45, name: 'Christopher Reed', description: 'Dog and bird care enthusiast', pets: ['Dog', 'Bird'], rating: 4.9, location: 'Oakland' },
+    // { id: 46, name: 'Sarah Cook', description: 'Experienced with aquatic pets', pets: ['Fish'], rating: 4.6, location: 'Minneapolis' },
+    // { id: 47, name: 'David Bell', description: 'Cat and small mammals lover', pets: ['Cat', 'Hamster'], rating: 4.8, location: 'Tulsa' },
+    // { id: 48, name: 'Susan Murphy', description: 'Specializes in reptiles', pets: ['Snake', 'Lizard'], rating: 4.7, location: 'Arlington' },
+    // { id: 49, name: 'James Bailey', description: 'Experienced with farm animals', pets: ['Horse', 'Goat'], rating: 4.9, location: 'Tampa' },
+    // { id: 50, name: 'Karen Rivera', description: 'Birds and small pets expert', pets: ['Bird', 'Hamster'], rating: 4.6, location: 'New Orleans' },
+    // { id: 51, name: 'Nancy Brooks', description: 'Experienced with cats and dogs', pets: ['Cat', 'Dog'], rating: 4.8, location: 'Wichita' },
+    // { id: 52, name: 'Gregory Powell', description: 'Professional sitter for exotic pets', pets: ['Snake', 'Fish'], rating: 4.9, location: 'Bakersfield' },
+    // { id: 53, name: 'Angela Bennett', description: 'Great with reptiles and amphibians', pets: ['Turtle', 'Lizard'], rating: 4.7, location: 'Cleveland' },
+    // { id: 54, name: 'George Russell', description: 'Loves taking care of birds and rabbits', pets: ['Bird', 'Rabbit'], rating: 4.6, location: 'Aurora' },
+    // { id: 55, name: 'Sharon Fisher', description: 'Dog and cat lover', pets: ['Dog', 'Cat'], rating: 4.8, location: 'Anaheim' },
+    // { id: 56, name: 'Patrick Peterson', description: 'Expert in exotic animals', pets: ['Snake', 'Lizard'], rating: 4.9, location: 'Santa Ana' },
+    // { id: 57, name: 'Michelle Simmons', description: 'Birds and small animals enthusiast', pets: ['Bird', 'Hamster'], rating: 4.6, location: 'Corpus Christi' },
+    // { id: 58, name: 'Larry Richardson', description: 'Experienced with farm animals', pets: ['Horse', 'Goat'], rating: 4.8, location: 'Riverside' },
+    // { id: 59, name: 'Sandra Hughes', description: 'Small mammals and birds lover', pets: ['Rabbit', 'Bird'], rating: 4.7, location: 'Lexington' },
+    // { id: 60, name: 'Timothy Jenkins', description: 'Professional sitter for aquatic pets', pets: ['Fish'], rating: 4.9, location: 'Stockton' },
   ];
   // Filter personnel based on selected pets and user location
   const filteredPersonnel = personnel.filter(person =>
-    person.pets.some(pet => selectedPets.includes(pet)) && person.location === userLocation
+    person.pets.some(pet => selectedPets.includes(pet)) || (person.location != null && person.location === userLocation)
   );
 
   // Additional personnel available for other pets
@@ -81,73 +81,106 @@ const SearchResults = () => {
      !filteredPersonnel.includes(person)
   );
 
+ 
   return (
-    <div className="p-4">
-      <h1 className="text-4xl font-bold mb-4">Search Results</h1>
+    <div className="p-6 m-6">
+      <h1 className="text-4xl font-bold mb-4 flex justify-center mt-4">Search Results</h1>
       {filteredPersonnel.length > 0 ? (
-        <p className="mb-2">{filteredPersonnel.length} search results found in <strong>{userLocation}</strong> for the following pets:</p>
+        <p className="mb-2 flex justify-center">
+          {filteredPersonnel.length} search results found in {' '}
+          <strong>{userLocation}</strong> for the following pets:
+        </p>
       ) : (
-        <p className="mb-2">No search results found in <strong>{userLocation}</strong> for the selected pets.</p>
+        <p className="mb-2 flex justify-center">
+          No search results found in <strong>{userLocation}</strong> for the
+          selected pets.
+        </p>
       )}
 
-
-<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-  {filteredPersonnel.map(person => (
-    <div key={person.id} className="bg-white rounded-lg overflow-hidden shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-      <div className="bg-gradient-to-r from-green-400 to-pink-500 text-center py-2">
-        <h2 className="text-xl font-semibold text-white">{person.name}</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {filteredPersonnel.map((person) => (
+          <div
+            key={person.id}
+            className="bg-white m-5 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+          >
+            <div className="px-4 py-2 bg-gray-200 border-b border-gray-300 flex items-center">
+              <img
+                src={avatar}
+                alt={person.name}
+                className="w-12 h-12 rounded-full mr-2"
+              />
+              <h2 className="text-xl font-semibold text-gray-800">
+                {person.name}
+              </h2>
+            </div>
+            <div className="p-4">
+              <p className="text-gray-800">{person.description}</p>
+              <div className="flex items-center mt-2">
+                <FaStar className="text-yellow-500" />
+                <span className="text-gray-700 ml-1">{person.rating}</span>
+              </div>
+              <div className="flex items-center mt-2">
+                <FaMapMarkerAlt className="text-gray-500" />
+                <span className="text-gray-700 ml-1">
+                  {person.location}
+                </span>
+              </div>
+              <div className="flex items-center mt-4">
+                <FaPhone className="text-gray-500" />
+                <button className="text-purple-500 ml-2 font-semibold hover:underline">
+                  Contact Us
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-      <div className="p-4">
-        <p className="text-gray-800">{person.description}</p>
-        <div className="flex items-center mt-2">
-          <FaStar className="text-yellow-500" />
-          <span className="text-gray-700 ml-1">{person.rating}</span>
-        </div>
-        <div className="flex items-center mt-2">
-          <FaMapMarkerAlt className="text-gray-500" />
-          <span className="text-gray-700 ml-1">{person.location}</span>
-        </div>
-        <div className="flex items-center mt-4">
-          <FaPhone className="text-gray-500" />
-          <button className="text-purple-500 ml-2 font-semibold hover:underline">Contact Us</button>
-        </div>
-      </div>
-    </div>
-  ))}
-</div>
-
-
-
 
       {additionalPersonnel.length > 0 && (
         <>
-          <p className="mt-6 text-2xl font-bold">Explore more pet sitters in <strong>{userLocation}</strong>:</p>
+<p className="mt-6 mb-8 text-2xl font-bold flex items-center justify-center">
+  Explore more pet sitters :
+</p>
 
-<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-  {additionalPersonnel.map(person => (
-    <div key={person.id} className="bg-white rounded-lg overflow-hidden shadow-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-      <div className="bg-gradient-to-r from-green-400 to-pink-500 text-center py-2">
-        <h2 className="text-xl font-semibold text-white">{person.name}</h2>
-      </div>
-      <div className="p-4">
-        <p className="text-gray-800">{person.description}</p>
-        <div className="flex items-center mt-2">
-          <FaStar className="text-yellow-500" />
-          <span className="text-gray-700 ml-1">{person.rating}</span>
-        </div>
-        <div className="flex items-center mt-2">
-          <FaMapMarkerAlt className="text-gray-500" />
-          <span className="text-gray-700 ml-1">{person.location}</span>
-        </div>
-        <div className="flex items-center mt-4">
-          <FaPhone className="text-gray-500" />
-          <button className="text-purple-500 ml-2 font-semibold hover:underline">Contact Us</button>
-        </div>
-      </div>
-    </div>
-  ))}
-</div>
 
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {additionalPersonnel.map((person) => (
+              <div
+                key={person.id}
+                className="bg-white m-5 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+              >
+                <div className="px-4 py-2 bg-gray-200 border-b border-gray-300 flex items-center">
+                  <img
+                    src={avatar}
+                    alt={person.name}
+                    className="w-12 h-12 rounded-full mr-2"
+                  />
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {person.name}
+                  </h2>
+                </div>
+                <div className="p-4">
+                  <p className="text-gray-800">{person.description}</p>
+                  <div className="flex items-center mt-2">
+                    <FaStar className="text-yellow-500" />
+                    <span className="text-gray-700 ml-1">{person.rating}</span>
+                  </div>
+                  <div className="flex items-center mt-2">
+                    <FaMapMarkerAlt className="text-gray-500" />
+                    <span className="text-gray-700 ml-1">
+                      {person.location}
+                    </span>
+                  </div>
+                  <div className="flex items-center mt-4">
+                    <FaPhone className="text-gray-500" />
+                    <button className="text-purple-500 ml-2 font-semibold hover:underline">
+                      Contact Us
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </>
       )}
     </div>
